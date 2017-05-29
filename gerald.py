d1f51f47458294853f780d125dc89fe0d4a048e3
joeychik <joey.chik0103@gmail.com>
2017-05-29 14:36:40 -0400

    waow
    dicc

--- gerald.py
+++ gerald.py
@@ -1,4 +1,4 @@
-#The "Amazing Bario Cousins" program
+#the#The "Amazing Bario Cousins" program
 #a side-scrolling 2D platform-based game inspired by the classic video game Super Mario Brothers
 #Jason Yeung and Joey Chik
 #created: 2017-05-25
@@ -20,26 +20,18 @@ clock = pygame.time.Clock()
 
 #define classes
 class Player(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([])
-
 
 
 #loop until user clicks close button
 done = False
 
 while done == False:
-    #event processing. all events such as key presses or clicks are events. this is the section that processes events(takes input)
+    #event processing
     for event in pygame.event.get():
-        #check if window close button has been pressed
         if event.type == pygame.QUIT:
             done = True
-
     #game logic
 
-
-    #game drawing
-
+    #drawing graphics
 
 pygame.quit()
